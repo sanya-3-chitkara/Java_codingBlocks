5e5860ca15eca14154293b15abb6f208779d698d
Sanya Singla <78159088+sanya-3-chitkara@users.noreply.github.com>
2023-02-26 00:43:03 +0530

    Create Sliding Window Maximum.java

---
+++ Sliding Window Maximum.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Main {
+
+
+    public static int[] maxSlidingWindow(int[] a,int k) {
+        //Write your code here
+        int n = a.length;
+        int[] r = new int[n - k + 1];
+        int ri = 0;
+        // store index
+        Deque < Integer > q = new ArrayDeque < > ();
+        for (int i = 0; i < a.length; i++) {
+            // remove numbers out of range k
+            if (!q.isEmpty() && q.peek() == i - k) {
+                q.poll();
+            }
+            // remove smaller numbers in k range as they are useless
+            while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+                q.pollLast();
+            }
+
+            q.offer(i);
+            if (i >= k - 1) {
+                r[ri++] = a[q.peek()];
+            }
+        }
+        return r;
+    }
+
+
+
+
+    public static void main (String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        arr = maxSlidingWindow(arr,k);
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+}
